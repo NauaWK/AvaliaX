1dd989d18ddfd691280a8405498612c81f0b2408
GTDSantos <tilted2266@gmail.com>
2026-05-28 18:35:24 -0300

    Logout e lista de avaliações

--- front/static/js/pacientes.js
+++ front/static/js/pacientes.js
@@ -1,3 +1,28 @@
+const token = localStorage.getItem("token");
+
+if (!token) {
+    window.location.href = "login.html"
+}
+//Nome usuario
+const payload = JSON.parse(atob(token.split('.')[1]));
+
+const nome = payload.sub;
+
+const nomeUser = document.getElementById("info-user")
+
+nomeUser.innerText = nome
+
+//logout
+const btnLogout = document.getElementById("logout")
+
+btnLogout.addEventListener("click", logout)
+
+function logout() {
+    localStorage.removeItem("token")
+
+    window.location.href = "login.html"
+}
+
 
 // abrir e fechar o modal (tela do formulario)
 const abrirModal = document.getElementById("btn");
